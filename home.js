fe103eae22c02b9f0bbd82516b4a9f85500856ba
wscanf <wscanf@126.com>
2016-05-20 23:11:00 +0800

    Update home.js

--- home.js
+++ home.js
@@ -1,4 +1,13 @@
 function run() {
-		alert("going home");
+	if ($('#_myoQL_unshippedOrders a').text().indexOf('0 ') == 0) {
+		setTimeout(function(){
+			window.location.href=window.location.href;
+		}, 1000);
+	} else {
+		$('option[value="bulk_confirm"]').attr('selected','selected');
+		$('.checkall').click();
+		MYO.JS.check_all($('.checkall').attr('checked'));
+		$('.action-go').click();
+	}
 }
 run();
