45e071658c1dd90ea68cad30717fbd57712d980c
wscanf <wscanf@126.com>
2016-05-20 23:26:23 +0800

    Update bulk.js

--- bulk.js
+++ bulk.js
@@ -6,5 +6,6 @@ if (__use_local__) {
   $('input[name="trackingID_UNSHIPPEDITEMS"]').each(function(){
     var code = (Math.random() > 0.5 ? 'KA' : 'LA');
     $(this).val(code + parseInt(Math.random() * 100000000000));
+    $('#myo-cms-confirm').click();
   });
 }
