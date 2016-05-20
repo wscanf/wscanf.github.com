36ac5758d286cdfa9fbb06a80602ffb1d305d6e2
wscanf <wscanf@126.com>
2016-05-20 23:22:38 +0800

    Create bulk.js

---
+++ bulk.js
@@ -0,0 +1,10 @@
+var __use_local__ = true;
+
+$('#carrierNameDropDown_UNSHIPPEDITEMS option:eq(1)').attr('selected','selected');
+
+if (__use_local__) {
+  $('input[name="trackingID_UNSHIPPEDITEMS"]').each(function(){
+    var code = (Math.random() > 0.5 ? 'KA' : 'LA');
+    $(this).val(code + parseInt(Math.random() * 100000000000));
+  });
+}
